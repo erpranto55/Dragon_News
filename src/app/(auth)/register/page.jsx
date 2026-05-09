c6257a47eaad45fdc4de58b2a356c62f2098032b
erpranto <erpranto55@gmail.com>
2026-05-09 12:56:50 +0600

    Password Toogle Modified

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import {
     Button,
     FieldError,
@@ -21,6 +21,9 @@ import { useRouter } from "next/navigation";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const RegisterPage = () => {
+
+    const [isShowPassword, setIsShowPassword] = useState(false);
+
 
     const {
         register,
